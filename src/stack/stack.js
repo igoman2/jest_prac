eba7bddf81f8c55b2d40dbd643a5acbc0419900b
junho.han <junho.han@ebridge-world.com>
2021-12-09 20:19:12 +0900

    Stack with listnode

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -1,25 +1,32 @@
 class Stack {
   constructor() {
-    this.array = [];
+    this._size = 0;
+    this.head = null;
   }
 
   size() {
-    return this.array.length;
+    return this._size;
   }
-  push(val) {
-    this.array.push(val);
+  push(item) {
+    const node = { item, next: this.head };
+    this.head = node;
+    this._size++;
   }
   pop() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error("Stack is empty");
     }
-    return this.array.pop();
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.item;
   }
   peek() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error("Stack is empty");
     }
-    return this.array[this.array.length - 1];
+    const node = this.head;
+    return node.item;
   }
 }
 module.exports = Stack;
